80cc411e9eaa1ab5d7362ac2fa2baa6e0c2d36ee
豊原宗一郎 <toyohara.s.aa@m.titech.ac.jp>
2017-11-16 18:39:23 +0900

    Just wrote models

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import os
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Reshape
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv3D
+from keras.layers.convolutional import Conv2D, Conv3D, UpSampling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Flatten, Dropout
 
@@ -19,20 +19,63 @@ G_FRAMES = 4
 D_FRAMES = 5
 
 
-def generator():
+def make_generator():
     model = Sequential([
-        Conv3D(64,          #filters
-               kernel_size=(4, 4, 4),
-               strides=(2, 2, 2),
+        # The 5 following convolutions are from
+        # https://www.semanticscholar.org/paper/Generating-the-Future-with-Adversarial-Transformer-Vondrick-Torralba/263d82ed38ddf2940c64ef74de3cdcaf76f1082d
+        Conv3D(32,          #filters
+               kernel_size=(1, 3, 3),
+               strides=(1, 1, 1),
+               padding='same',
+               dilation_rate=(1, 1, 1),
+               input_shape=(G_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
+        Conv3D(64,  # filters
+               kernel_size=(1, 3, 3),
+               strides=(1, 1, 1),
+               padding='same',
+               dilation_rate=(1, 2, 2),
+               input_shape=(G_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
+        Conv3D(128,  # filters
+               kernel_size=(1, 3, 3),
+               strides=(1, 1, 1),
+               padding='same',
+               dilation_rate=(1, 4, 4),
+               input_shape=(G_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
+        Conv3D(256,  # filters
+               kernel_size=(1, 3, 3),
+               strides=(1, 1, 1),
+               padding='same',
+               dilation_rate=(1, 8, 8),
+               input_shape=(G_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
+        Conv3D(32,  # filters
+               kernel_size=(G_FRAMES, 1, 1),
+               strides=(1, 1, 1),
                padding='same',
                input_shape=(G_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
-        Dense(units=10, input_dim=28),
-        Activation('softmax')
+
+        # followings are original
+        LeakyReLU(0.2),
+        Flatten(),
+        Dense(61*61*128),
+        BatchNormalization(),
+        Activation('relu'),
+        Reshape((61, 61, 128), input_shape=(61*61*128)),
+        UpSampling2D((2, 2)),
+        Conv2D(64, (5, 5), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        UpSampling2D((2, 2)),
+        Conv2D(32, (5, 5), padding='same'),
+        BatchNormalization(),
+        Activation('relu'),
+        UpSampling2D((2, 2)),
+        Conv2D(16, (5, 5), padding='same'),
+        Activation('tanh')
     ])
 
     return model
 
-def discriminator():
+def make_discriminator():
     model = Sequential([
         Conv3D(64,  # filters
                kernel_size=(4, 4, 4),
@@ -44,16 +87,19 @@ def discriminator():
                kernel_size=(4, 4, 4),
                strides=(2, 2, 2),
                padding='same',),
+        BatchNormalization(),
         LeakyReLU(0.2),
         Conv3D(256,  # filters
                kernel_size=(4, 4, 4),
                strides=(2, 2, 2),
                padding='same', ),
+        BatchNormalization(),
         LeakyReLU(0.2),
         Conv3D(512,  # filters
                kernel_size=(4, 4, 4),
                strides=(2, 2, 2),
                padding='same', ),
+        BatchNormalization(),
         LeakyReLU(0.2),
         Flatten(),
         Dense(256),
--- train.py
+++ train.py
@@ -1,12 +1,16 @@
 import keras
+from keras.models import Sequential
 from keras.optimizers import RMSprop
 import keras.callbacks
+from keras.optimizers import Adam
 
 import time
 
 from load_video import load_data
 
+from models import make_generator, make_discriminator
 
+# Hyper paremeters
 NUM_OF_DATA = 22
 BATCH_SIZE = 32
 NUM_EPOCH = 20
@@ -14,6 +18,18 @@ NUM_EPOCH = 20
 def train():
     data = load_data(NUM_OF_DATA)
     print(data.shape)
+
+    # Make discriminator
+    discriminator = make_discriminator()
+    d_optimizer =Adam(lr=1e-5, beta_1=0.1)
+    discriminator.compile(loss='binary_crossentropy', optimizer=d_optimizer)
+
+    discriminator.trainable = False
+    generator = make_generator()
+    dcgan = Sequential([generator, discriminator])
+    g_optimizer = Adam(lr=2e-4, beta_1=0.5)
+    dcgan = compile(loss='binary_crossentropy', optimizer=g_optimizer)
+
 
     # tensorboardの出力
     save_tb_cb = keras.callbacks.TensorBoard(
