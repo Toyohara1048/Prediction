f13137befc3507d40d6a2bde5df8a9cb6ee57c7a
豊原宗一郎 <toyohara.s.aa@m.titech.ac.jp>
2017-11-16 17:20:15 +0900

    Added Discriminator

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import os
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Reshape
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import UpSampling2D, Convolution2D, Conv2D, Conv3D
+from keras.layers.convolutional import Conv3D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Flatten, Dropout
 
@@ -35,13 +35,32 @@ def generator():
 def discriminator():
     model = Sequential([
         Conv3D(64,  # filters
-               kernel_size=(4, 4, 5),
+               kernel_size=(4, 4, 4),
                strides=(2, 2, 2),
                padding='same',
                input_shape=(D_FRAMES, LENGTH_OF_SIDE, LENGTH_OF_SIDE, 1)),
         LeakyReLU(0.2),
-
-
+        Conv3D(128,  # filters
+               kernel_size=(4, 4, 4),
+               strides=(2, 2, 2),
+               padding='same',),
+        LeakyReLU(0.2),
+        Conv3D(256,  # filters
+               kernel_size=(4, 4, 4),
+               strides=(2, 2, 2),
+               padding='same', ),
+        LeakyReLU(0.2),
+        Conv3D(512,  # filters
+               kernel_size=(4, 4, 4),
+               strides=(2, 2, 2),
+               padding='same', ),
+        LeakyReLU(0.2),
+        Flatten(),
+        Dense(256),
+        LeakyReLU(0.2),
+        Dropout(0.5),
+        Dense(1),
+        Activation('sigmoid')
     ])
 
     return model
