aa55b1c96af6ad454e2c538fcd6b3ea9925a72c6
豊原宗一郎 <toyohara.s.aa@m.titech.ac.jp>
2017-12-01 22:20:03 +0900

    Added predictor

--- predictor.py
+++ predictor.py
@@ -53,13 +53,20 @@ def prediction(index):
     for i in range(4):
         temp = cv2.cvtColor(images[i], cv2.COLOR_GRAY2BGR)
         cv2.circle(temp, (int(centers_X[index][2*i] * temp.shape[0]), int(centers_X[index][2*i+1] * temp.shape[1])), 10, (255, 0, 0), -1)
+        rad = math.atan(tans_X[index][i])
+        end_pt = (int(centers_X[index][0] * temp.shape[0] * 50 * math.cos(rad)), int(centers_X[index][1] * temp.shape[1] * 50 * math.sin(rad)))
+        cv2.line(temp, (int(centers_X[index][2*i] * temp.shape[0]), int(centers_X[index][2*i+1] * temp.shape[1])), end_pt, (255, 255, 0), 1, 16);
         result = np.append(result, temp)
+
     temp = cv2.cvtColor(images[4], cv2.COLOR_GRAY2BGR)
-    cv2.circle(temp, (int(center[index][0] * temp.shape[0]), int(centers_X[index][1] * temp.shape[1])), 10, (0, 0, 255), -1)
+    cv2.circle(temp, (int(center[index][0] * temp.shape[0]), int(center[index][1] * temp.shape[1])), 10, (0, 0, 255), -1)
+    rad = math.atan(tan[index][0])
+    end_pt = (int(center[index][0] * temp.shape[0] * 50 * math.cos(rad)),
+              int(center[index][1] * temp.shape[1] * 50 * math.sin(rad)))
+    cv2.line(temp, (int(center[index][0] * temp.shape[0]), int(center[index][1] * temp.shape[1])),
+             end_pt, (0, 255, 255), 1, 16);
     result = np.append(result, temp)
     result = result.reshape(5, 488, 488, 3)
-
-    print(result.shape)
 
     result_image = combine_images(result)
     quarter = cv2.resize(result_image, None, fx=1 / 2, fy=1 / 2)
